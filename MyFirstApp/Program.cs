d30ae78a5eab0449e8a444509306a9deffb37de4
KristjanKaa <159649951+KristjanKaa@users.noreply.github.com>
2024-02-21 20:17:25 +0200

    first commit

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// Minu kommentaarid, Kristjan
+
+// rakendus küsib kasutajal sisestada tema nime 
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");  //Output
+//String- sõne
+string userName = Console.ReadLine();   //Input
+
+
+Console.WriteLine("Hello" + ", " + userName + "!");  //Output
+//string interpolation
+
+Console.WriteLine($"Hello, {userName} !");      //Output
